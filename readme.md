dfd59926b5e49a004d383a5929941de9e9fc6fc2
173gvQY8MFaDk9Jt <g@v.q>
2025-01-14 00:16:57 +0800

    first commit

--- readme.md
+++ readme.md
@@ -3,3 +3,9 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-vercel-satori-png-service-font-honor-sans-cn?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-vercel-satori-png-service-font-honor-sans-cn)
 
 Provide HONOR Sans CN fonts for the vercel-satori-png-service
+
+---
+
+[HONOR Sans CN](https://developer.honor.com/cn/doc/guides/100681)
+
+![example.png](example.png)
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,60 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Service } from "koishi";
+import type { Font, FontWeight, FontStyle } from "satori";
+import fs from "node:fs/promises";
+import path from "node:path";
+// noinspection ES6UnusedImports
+import {} from "koishi-plugin-vercel-satori-png-service";
 
-export const name = 'vercel-satori-png-service-font-honor-sans-cn'
+const serviceName = "vercelSatoriPngServiceFontHonorSansCn";
 
-export interface Config {}
+class VercelSatoriPngServiceFontHonorSansCn extends Service {
+  private _ctx: Context;
+  constructor(
+    ctx: Context,
+    config: VercelSatoriPngServiceFontHonorSansCn.Config,
+  ) {
+    super(ctx, serviceName);
+    this._ctx = ctx;
+  }
 
-export const Config: Schema<Config> = Schema.object({})
+  protected async start(): Promise<void> {
+    await this.initFonts();
+  }
+  private async initFonts() {
+    const fonts: Font[] = [];
+    for (let i = 0; i < this.fontNames.length; i++) {
+      const fontName = this.fontNames[i];
+      fonts.push({
+        name: "HONOR Sans CN",
+        weight: ((i + 1) * 100) as FontWeight,
+        style: "normal" as FontStyle,
+        data: await fs.readFile(
+          path.join(__dirname, `../fonts/HONORSansCN-${fontName}.ttf`),
+        ),
+      });
+    }
+    this._ctx.vercelSatoriPngService.addFont(fonts);
+  }
 
-export function apply(ctx: Context) {
-  // write your plugin here
+  private fontNames: string[] = [
+    "Thin",
+    "ExtraLight",
+    "Light",
+    "Regular",
+    "Medium",
+    "DemiBold",
+    "Bold",
+    "ExtraBold",
+    "Heavy",
+  ];
 }
+
+namespace VercelSatoriPngServiceFontHonorSansCn {
+  export const inject = ["vercelSatoriPngService"];
+
+  export const usage =
+    '<a target="_blank" href="https://developer.honor.com/cn/doc/guides/100681">HONOR Sans CN</a>';
+  export interface Config {}
+  export const Config: Schema<Config> = Schema.object({});
+}
+export default VercelSatoriPngServiceFontHonorSansCn;
